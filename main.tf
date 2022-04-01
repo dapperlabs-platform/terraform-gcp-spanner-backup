176467697a7b6391ae06b6e8b38c892e96ab5626
Femi Oladele <femi.oladele@sharkmob.com>
2022-04-01 10:13:09 +0200

    Add Base backup module

---
+++ main.tf
@@ -0,0 +1,87 @@
+# Service Accounts
+resource "google_service_account" "backup_sa" {
+  account_id   = "backup-sa"
+  display_name = "Spanner Backup Function Service Account"
+}
+
+resource "google_project_iam_member" "backup_sa_spanner_iam" {
+  project = var.gcp_project_id
+  role    = "roles/spanner.backupWriter"
+
+  member = "serviceAccount:${google_service_account.backup_sa.email}"
+}
+
+# Cloud Pub/Sub
+resource "google_pubsub_topic" "backup_topic" {
+  for_each = var.database_ids
+  project  = var.gcp_project_id
+  name     = "${each.value}-${var.pubsub_topic}"
+}
+
+resource "google_pubsub_topic_iam_member" "backup_sa_pubsub_sub_iam" {
+  for_each = var.database_ids
+  project  = var.gcp_project_id
+  topic    = "${each.value}-${var.pubsub_topic}"
+  role     = "roles/pubsub.subscriber"
+  member   = "serviceAccount:${google_service_account.backup_sa.email}"
+}
+
+# Cloud Scheduler
+resource "google_app_engine_application" "app" {
+  project     = var.gcp_project_id
+  location_id = var.location
+}
+
+resource "google_cloud_scheduler_job" "backup_job" {
+  for_each    = var.database_ids
+  region      = var.region
+  project     = var.gcp_project_id
+  name        = "${each.value}-spanner-backup-job"
+  description = "Backup job for database - ${each.value}"
+  schedule    = var.schedule
+  time_zone   = var.time_zone
+  pubsub_target {
+    topic_name = google_pubsub_topic.backup_topic[each.value].id
+    data       = base64encode("{\"Database\":\"projects/${var.gcp_project_id}/instances/${var.spanner_instance_id}/databases/${each.value}\", \"Expire\": \"6h\"}")
+  }
+  depends_on = [google_app_engine_application.app]
+}
+
+# Cloud Function
+resource "google_storage_bucket" "bucket_gcf_source" {
+  name                        = "${var.gcp_project_id}-gcp-ssb-source"
+  storage_class               = "REGIONAL"
+  location                    = var.region
+  force_destroy               = "true"
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+}
+
+data "archive_file" "local_backup_source" {
+  type        = "zip"
+  source_dir  = abspath("${path.module}/../../backup")
+  output_path = "${var.local_output_path}/backup.zip"
+}
+
+resource "google_storage_bucket_object" "gcs_functions_backup_source" {
+  name   = "backup.${data.archive_file.local_backup_source.output_md5}.zip"
+  bucket = google_storage_bucket.bucket_gcf_source.name
+  source = data.archive_file.local_backup_source.output_path
+}
+
+resource "google_cloudfunctions_function" "spanner_backup_function" {
+  for_each            = var.database_ids
+  name                = "${each.value}-SpannerCreateBackup"
+  project             = var.gcp_project_id
+  region              = var.region
+  available_memory_mb = "256"
+  entry_point         = "SpannerCreateBackup"
+  runtime             = "go113"
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.backup_topic[each.value].id
+  }
+  source_archive_bucket = google_storage_bucket.bucket_gcf_source.name
+  source_archive_object = google_storage_bucket_object.gcs_functions_backup_source.name
+  service_account_email = google_service_account.backup_sa.email
+  depends_on            = [google_pubsub_topic.backup_topic]
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "gcp_project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "uniform_bucket_level_access" {
+  type    = bool
+  default = false
+}
+
+variable "schedule" {
+  type        = string
+  description = "schedule on which you want to call the cloud scheduler job. The below is set to run every 1 hour"
+  default     = "0 * * * *"
+  //Every Hour
+}
+
+variable "time_zone" {
+  type    = string
+  default = "America/Vancouver"
+}
+
+variable "pubsub_topic" {
+  type = string
+}
+
+variable "local_output_path" {
+  type    = string
+  default = "build"
+}
+
+variable "database_ids" {
+  type        = set(string)
+  description = "Spanner Databases you want to backup"
+}
+
+variable "spanner_instance_id" {
+  type        = string
+  description = "Spanner Instance ID where you database is located that you want to backup"
+}
+
+variable "location" {
+  type        = string
+  description = "location for App Engine"
+}
