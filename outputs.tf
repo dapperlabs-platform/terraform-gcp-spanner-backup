bba58a3a98230e41f3f0bb90a6776570a5e03d6d
Ben Noyce <ben@noyce.com>
2023-06-23 09:07:06 -0600

    Comment out workflow outputs

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "workflow_id" {
-  description = "The id of the workflow."
-  value       = module.workflow.workflow_id
-}
+# output "workflow_id" {
+#   description = "The id of the workflow."
+#   value       = module.workflow.workflow_id
+# }
 
-output "scheduler_job_id" {
-  description = "Google Cloud scheduler job id"
-  value       = module.workflow.scheduler_job_id
-}
+# output "scheduler_job_id" {
+#   description = "Google Cloud scheduler job id"
+#   value       = module.workflow.scheduler_job_id
+# }
 
 output "backup_args" {
   description = "The arguments to pass to the backup workflow"
