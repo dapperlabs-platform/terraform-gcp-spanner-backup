26e01748b859594f5cae686151dc84b01398bb1f
Ben Noyce <ben@noyce.com>
2023-06-23 08:35:51 -0600

    Updates backup to leverage workflows + spanner backup plumbing

--- main.tf
+++ main.tf
@@ -1,92 +1,58 @@
-resource "random_id" "suffix" {
-  byte_length = 8
+locals {
+  backup_name = "${var.instance_name}-backup"
+  workflow_argument = jsonencode([
+    for v in var.database_names : {
+      backupId    = "${var.instance_name}-${k}-backup",
+      database    = "projects/${var.project_name}/instances/${var.instance_name}/databases/${v}",
+      expire_time = var.backup_expire_time,
+      parent      = "projects/${var.project_name}/instances/${var.instance_name}",
+  }])
 }
 
-# Service Accounts
-resource "google_service_account" "backup_sa" {
-  account_id   = "backup-sa-${random_id.suffix.hex}"
-  display_name = "Spanner Backup Function Service Account"
-}
-
-resource "google_project_iam_member" "backup_sa_spanner_iam" {
-  project = var.gcp_project_id
-  role    = "roles/spanner.backupWriter"
-
-  member = "serviceAccount:${google_service_account.backup_sa.email}"
-}
-
-# Cloud Pub/Sub
-resource "google_pubsub_topic" "backup_topic" {
-  for_each = var.database_ids
-  project  = var.gcp_project_id
-  name     = "${each.value}-${var.pubsub_topic}"
-}
-
-resource "google_pubsub_topic_iam_member" "backup_sa_pubsub_sub_iam" {
-  for_each = var.database_ids
-  project  = var.gcp_project_id
-  topic    = "${each.value}-${var.pubsub_topic}"
-  role     = "roles/pubsub.subscriber"
-  member   = "serviceAccount:${google_service_account.backup_sa.email}"
-}
-
-# Cloud Scheduler
-resource "google_app_engine_application" "app" {
-  count       = var.create_app_engine_app ? 1 : 0
-  project     = var.gcp_project_id
-  location_id = var.location
-}
-
-resource "google_cloud_scheduler_job" "backup_job" {
-  for_each    = var.database_ids
-  region      = var.region
-  project     = var.gcp_project_id
-  name        = "${each.value}-spanner-backup-job-${random_id.suffix.hex}"
-  description = "Backup job for database - ${each.value}"
-  schedule    = var.schedule
-  time_zone   = var.time_zone
-  pubsub_target {
-    topic_name = google_pubsub_topic.backup_topic[each.value].id
-    data       = base64encode("{\"Database\":\"projects/${var.gcp_project_id}/instances/${var.spanner_instance_id}/databases/${each.value}\", \"Expire\": \"6h\"}")
+module "scheduler_service_account" {
+  source     = "github.com/dapperlabs-platform/terraform-google-iam-service-account?ref=v1.1.8"
+  project_id = var.project_name
+  name       = "${local.backup_name}-scheduler"
+  iam_project_roles = {
+    "${var.project_name}" = [
+      "roles/workflows.invoker"
+    ]
   }
-  depends_on = [google_app_engine_application.app]
 }
 
-# Cloud Function
-resource "google_storage_bucket" "bucket_gcf_source" {
-  name                        = "${var.gcp_project_id}-gcp-ssb-source-${random_id.suffix.hex}"
-  storage_class               = "REGIONAL"
-  location                    = var.region
-  force_destroy               = "true"
-  uniform_bucket_level_access = var.uniform_bucket_level_access
-}
-
-data "archive_file" "local_backup_source" {
-  type        = "zip"
-  source_dir  = abspath("${path.module}/backup")
-  output_path = "${var.local_output_path}/backup.zip"
-}
-
-resource "google_storage_bucket_object" "gcs_functions_backup_source" {
-  name   = "backup.${data.archive_file.local_backup_source.output_md5}.zip"
-  bucket = google_storage_bucket.bucket_gcf_source.name
-  source = data.archive_file.local_backup_source.output_path
-}
-
-resource "google_cloudfunctions_function" "spanner_backup_function" {
-  for_each            = var.database_ids
-  name                = "${each.value}-SpannerCreateBackup-${random_id.suffix.hex}"
-  project             = var.gcp_project_id
-  region              = var.region
-  available_memory_mb = "256"
-  entry_point         = "SpannerCreateBackup"
-  runtime             = "go113"
-  event_trigger {
-    event_type = "google.pubsub.topic.publish"
-    resource   = google_pubsub_topic.backup_topic[each.value].id
+module "workflow_service_account" {
+  source     = "github.com/dapperlabs-platform/terraform-google-iam-service-account?ref=v1.1.8"
+  project_id = var.project_name
+  name       = "${local.backup_name}-workflow"
+  iam_project_roles = {
+    "${var.project_name}" = [
+      "roles/spanner.backupAdmin"
+    ]
   }
-  source_archive_bucket = google_storage_bucket.bucket_gcf_source.name
-  source_archive_object = google_storage_bucket_object.gcs_functions_backup_source.name
-  service_account_email = google_service_account.backup_sa.email
-  depends_on            = [google_pubsub_topic.backup_topic]
+}
+
+module "workflow" {
+  source                 = "github.com/GoogleCloudPlatform/terraform-google-cloud-workflows?ref=v0.1.0"
+  project_id             = var.project_name
+  workflow_name          = "${local.backup_name}-workflow"
+  region                 = var.backup_schedule_region
+  service_account_email  = module.service_account_workflow.email
+  service_account_create = false
+
+  workflow_trigger = {
+    cloud_scheduler = {
+      name                  = "${local.backup_name}-job"
+      cron                  = var.backup_schedule
+      time_zone             = var.backup_time_zone
+      deadline              = var.backup_deadline
+      service_account_email = module.service_account_scheduler.email
+      argument              = var.workflow_argument
+    }
+  }
+  workflow_source = file("${path.module}/spanner_backup.yaml")
+
+  depends_on = [
+    module.service_account_scheduler,
+    module.service_account_workflow
+  ]
 }
