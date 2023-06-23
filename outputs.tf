e0bc978a0a73648d0ed6bed3fd077e71e8459913
Ben Noyce <ben@noyce.com>
2023-06-23 09:14:53 -0600

    Re-enable workflow

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
-# output "workflow_id" {
-#   description = "The id of the workflow."
-#   value       = module.workflow.workflow_id
-# }
+output "workflow_id" {
+  description = "The id of the workflow."
+  value       = module.workflow.workflow_id
+}
 
-# output "scheduler_job_id" {
-#   description = "Google Cloud scheduler job id"
-#   value       = module.workflow.scheduler_job_id
-# }
-
-output "backup_args" {
-  description = "The arguments to pass to the backup workflow"
-  value       = local.backup_args
+output "scheduler_job_id" {
+  description = "Google Cloud scheduler job id"
+  value       = module.workflow.scheduler_job_id
 }
