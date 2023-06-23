55d2ad4fe0bea810a0a3f6f5f0dd099ce8b71f9b
Ben Noyce <ben@noyce.com>
2023-06-23 08:42:47 -0600

    Enable flag

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "backup_deadline" {
   default     = "320s"
 }
 
+variable "backup_enabled" {
+  description = "Whether to enable the backup schedule"
+  type        = bool
+  default     = true
+}
+
 variable "backup_expire_time" {
   description = "Seconds until the backup expires"
   type        = number
