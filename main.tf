6a4f4088757689da6559296846ea5f68d428c1c0
Ben Noyce <ben@noyce.com>
2023-07-21 17:43:42 -0600

    Add instance alias name for generated naming errors

--- main.tf
+++ main.tf
@@ -7,12 +7,13 @@ locals {
       expireTime = var.backup_expire_time,
       parent     = "projects/${var.project_name}/instances/${var.instance_name}",
   }]
+  instance_alias_name = var.instance_alias_name != "" ? var.instance_alias_name : var.instance_name
 }
 
 module "scheduler_service_account" {
   source     = "github.com/dapperlabs-platform/terraform-google-iam-service-account?ref=v1.1.8"
   project_id = var.project_name
-  name       = "${var.instance_name}-scheduler"
+  name       = "${instance_alias_name}-scheduler"
 }
 
 resource "google_project_iam_member" "scheduler_workflow_invoker" {
@@ -24,7 +25,7 @@ resource "google_project_iam_member" "scheduler_workflow_invoker" {
 module "workflow_service_account" {
   source     = "github.com/dapperlabs-platform/terraform-google-iam-service-account?ref=v1.1.8"
   project_id = var.project_name
-  name       = "${var.instance_name}-workflow"
+  name       = "${instance_alias_name}-workflow"
 }
 
 resource "google_project_iam_member" "workflow_spanner_backup_admin" {
@@ -36,14 +37,14 @@ resource "google_project_iam_member" "workflow_spanner_backup_admin" {
 module "workflow" {
   source                 = "github.com/dapperlabs-platform/terraform-google-cloud-workflows?ref=v0.1.0"
   project_id             = var.project_name
-  workflow_name          = "${var.instance_name}-backup-workflow"
+  workflow_name          = "${instance_alias_name}-backup-workflow"
   region                 = var.backup_schedule_region
   service_account_email  = module.workflow_service_account.email
   service_account_create = false
 
   workflow_trigger = {
     cloud_scheduler = {
-      name                  = "${var.instance_name}-backup-job"
+      name                  = "${instance_alias_name}-backup-job"
       cron                  = var.backup_schedule
       time_zone             = var.backup_time_zone
       deadline              = var.backup_deadline
